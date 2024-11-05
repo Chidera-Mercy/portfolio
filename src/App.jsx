42f1c3672b536ee5896bbe34c907842772dd6155
Chidera-Mercy <derarose2006@gmail.com>
2024-11-05 13:26:50 +0000

    add fade

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components"
 import { theme } from "./utils/Theme"
-import { Container, MainBody } from "./styles/Global.styled"
+import { Container, MainBody, FadeImage } from "./styles/Global.styled"
 
 //import components
 import Showcase from "./components/Showcase"
@@ -9,17 +9,26 @@ import MyProjects from "./components/MyProjects"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 
+import TopFadeImage from './assets/top.png'
+import LeftFadeImage from './assets/left.png'
+import BottomFadeImage from './assets/bottom.png'
+import RightFadeImage from './assets/right.png'
+
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <MainBody>
         <Navbar />
+
         <Container>
           <Showcase />
           <MySkills />
           <MyProjects />
           <Footer />
         </Container>
+        <FadeImage src={TopFadeImage} top="0" />
+        <FadeImage src={LeftFadeImage} top="30vh" />
       </MainBody>
     </ThemeProvider>
   )
